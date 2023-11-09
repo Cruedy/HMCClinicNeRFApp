c58845d77c7215be6096d8cc660766affd130bbe
RinHa <rha@hmc.edu>
2023-11-08 16:42:57 -0800

    Add new views

---
+++ NeRFCapture/Views/IntroInstructionsView.swift
@@ -0,0 +1,8 @@
+//
+//  IntroInstructionsView.swift
+//  NeRFCapture
+//
+//  Created by Rin Ha on 11/8/23.
+//
+
+import Foundation
---
+++ NeRFCapture/Views/SendImagesToServerView.swift
@@ -0,0 +1,8 @@
+//
+//  SendImagesToServerView.swift
+//  NeRFCapture
+//
+//  Created by Rin Ha on 11/8/23.
+//
+
+import Foundation
